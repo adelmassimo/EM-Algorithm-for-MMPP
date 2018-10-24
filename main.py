9efbbb67429b7290c88dba43a76d616cf595afdc
adelmassimo <adel.massimo.ramadan@gmail.com>
2018-10-24 12:12:03 +0200

    steps forward

--- main.py
+++ main.py
@@ -1,136 +1,1 @@
-import numpy as np
-from numpy import linalg as LA
-from scipy.misc import factorial
-import model as md
-
-N = 3
-Observations = [(4, 1), (3, 1), (5, 2), (2,1), (9, 3), (11, 2), (8, 2), (20, 3), (17, 3), (18, 4)]
-D0 = np.matrix('-.4 .4 0; 0 -.5 .5; 0 0 -.6')
-D1 = np.eye(N)*.5
-M = np.matrix('.7 .2 .1 0; .3 .5 .2 0; .1 .2 .1 .6')
-
-model = md.Model()
-
-
-
-def uniformization(Q, q):
-	N = np.size(Q[1])
-	I = np.eye(N)
-	P = I - Q/q
-	return P
-
-
-def randomization( P, q, t ):
-	result = np.zeros((N,N))
-	#TODO use Fox and Glynn [FG88] to determinae l and r
-	l = 0
-	r = 500
-	for k in range(l, r):
-		e = np.exp(-q*t)*np.power(q*t, k)/factorial(k)
-		result = result + e * LA.matrix_power(P, k)
-	return result
-
-
-def getAlpha(i):
-	N = model.N
-	alpha = np.zeros( (1, N) )
-	alpha[0] = 1
-	
-	if i != 0:
-		P = randomization(model.P0, model.uniformization_rate, model.observations[i][0])
-	
-		alpha = np.multiply(
-			getAlpha(i-1) * P * model.P1,
-		 	np.transpose( model.M[:, model.observations[i][1]] )) 
-
-	return alpha/alpha.sum()
-
-
-def getBeta(i):
-	N = model.N
-	M = len(model.observations)
-	beta = np.ones( (N, 1) )
-	
-	if i < M-1:
-		P = randomization(model.P0, model.uniformization_rate, model.observations[i+1][0])
-		beta = np.multiply(
-			P * model.P1 * getBeta(i+1),
-			model.M[:, model.observations[i][1]] )
-		
-	return beta/beta.sum()
-
-
-def getSubV(i, l):
-	return getAlpha(i) * LA.matrix_power( model.P0, l )
-
-
-def getSubW(i, l):
-	return LA.matrix_power( model.P0, l ) * model.P1 * getBeta(i)
-
-
-def getV(i, r):
-	V = np.zeros(( model.N, r-1 ))
-	for j in range( r-1 ):
-		V[:, j] = getSubV(i, j)
-	return V
-
-
-def getW(i, r):
-	W = np.zeros(( r-1, model.N ))
-	for j in range( r-1 ):
-		W[r-2-j, :] = np.transpose( getSubW(i, j) )
-	return W
-
-########################################################################################################################
-
-
-
-
-
-epsilon = 10
-while epsilon > 0.1:
-	M = len(model.observations)
-	N = model.N
-	Z = np.zeros( (N, 4) )
-	X0 = np.zeros( (M, N, N) )
-	X1 = np.zeros( (M, N, N) )
-
-	for i in range( M - 1 ):
-		print i
-
-		X0_tmp = np.zeros( (N, N) )
-		X1_tmp = np.zeros( (N, 1) )
-		(l, r) = (0, 10)
-		a = getAlpha( i )
-		b = getBeta( i+1 )
-		q = model.uniformization_rate
-		t = model.observations[i][0]
-		Z[:, model.observations[i][1]] = Z[:, model.observations[i][1]] + a
-		
-		for k in range(l, r):
-			print k
-			e = np.exp(-q*t)*np.power(q*t, k)/factorial(k)
-			X0_tmp = X0_tmp + e * getV(i, r).dot(getW(i, r)) 
-			X1_tmp = X1_tmp + e * np.multiply(getSubV(i, r), np.transpose(b) )
-
-		X0[i,:,:] = X0_tmp.dot( model.P0 ) 
-		X1[i,:,:] = np.multiply(X1_tmp, model.P1 )
-		# print X1[i,:,:]
-
-	Y0 = X0.sum(axis=0)
-	Y1 = X1.sum(axis=0)
-	# print Y1
-
-	normFactor = Y0.sum(axis=1) + Y1.sum(axis=1)
-	Y0 = Y0 / normFactor[:, np.newaxis]
-	Y1 = Y1 / normFactor[:, np.newaxis]
-	Z = Z / Z.sum(axis=1)[:, np.newaxis]
-	
-	print(Y0)
-	print(Y1)
-	print(Z)
-	epsilon = max( LA.norm( model.P0 - Y0, 2),
-	    	 	   LA.norm( model.P1 - Y1, 2))
-	model.P0 = Y0
-	model.P1 = Y1 
-	model.M = Z
+import numpy as npfrom numpy import linalg as LAfrom scipy.misc import factorialimport model as mdfrom tqdm import tqdmmodel = md.Model()# def uniformization(Q, q):#     N = np.size(Q[1])#     I = np.eye(N)#     P = I - Q/q#     return Pdef randomization( P, q, t ):    result = np.zeros((N,N))    #TODO use Fox and Glynn [FG88] to determinae l and r    l = 0    r = 10    for k in range(l, r):        e = np.exp(-q*t)*np.power(q*t, k)/factorial(k)        result = result + e * LA.matrix_power(P, k)    return resultdef getAlpha(i):    N = model.N    alpha = np.zeros( (1, N) )    alpha[0] = 1    if i != 0:        P = randomization(model.P0, model.uniformization_rate, model.observations[i][0])        alpha = np.multiply(            getAlpha(i-1) * P * model.P1,            np.transpose( model.M[:, model.observations[i][1]] ))    return alpha/alpha.sum()def getBeta(i):    N = model.N    M = len(model.observations)    beta = np.ones( (N, 1) )    if i < M-1:        P = randomization(model.P0, model.uniformization_rate, model.observations[i+1][0])        beta = np.multiply(            P * model.P1 * getBeta(i+1),            model.M[:, model.observations[i][1]] )    return beta/beta.sum()def getSubV(i, l):    return getAlpha(i) * LA.matrix_power( model.P0, l )def getSubW(i, l):    return LA.matrix_power( model.P0, l ) * model.P1 * getBeta(i)def getV(i, r):    V = np.zeros(( model.N, r-1 ))    for j in range( r-1 ):        V[:, j] = getSubV(i, j)    return Vdef getW(i, r):    W = np.zeros(( r-1, model.N ))    for j in range( r-1 ):        W[r-2-j, :] = np.transpose( getSubW(i, j) )    return W########################################################################################################################if __name__ == '__main__':    epsilon = 10    print(model.P0)    print(model.P1)    print(model.M)    while epsilon > 0.06:        M = len(model.observations)        N = model.N        Z = np.zeros( (N, 4) )        X0 = np.zeros( (M, N, N) )        X1 = np.zeros( (M, N, N) )        for i in tqdm(range( M )):            # print i            X0_tmp = np.zeros( (N, N) )            X1_tmp = np.zeros( (N, 1) )            (l, r) = (0, 15)            a = getAlpha( i )            b = getBeta( i+1 )            q = model.uniformization_rate            t = model.observations[i][0]            Z[:, model.observations[i][1]] = Z[:, model.observations[i][1]] + a            for k in range(l, r):                e = np.exp(-q*t)*np.power(q*t, k)/factorial(k)                X0_tmp = X0_tmp + e * getV(i, r).dot(getW(i, r))                X1_tmp = X1_tmp + e * np.multiply(getSubV(i, r), np.transpose(b) )            X0[i,:,:] = X0_tmp.dot( model.P0 )            X1[i,:,:] = np.multiply(X1_tmp, model.P1 )            # print X1[i,:,:]        Y0 = X0.sum(axis=0)        Y1 = X1.sum(axis=0)        # print Y1        normFactor = Y0.sum(axis=1) + Y1.sum(axis=1)        Y0 = Y0 / normFactor[:, np.newaxis]        Y1 = Y1 / normFactor[:, np.newaxis]        Z = Z / Z.sum(axis=1)[:, np.newaxis]        print(Y0)        print(Y1)        print(Z)        epsilon = max( LA.norm( model.P0 - Y0, 2),                       LA.norm( model.P1 - Y1, 2))        model.P0 = np.matrix( Y0 )        model.P1 = np.matrix( Y1 )        model.M = np.matrix( Z )    print( model.P0 )    print( model.P1 )    print( model.M )
--- model.py
+++ model.py
@@ -3,26 +3,34 @@ from numpy import linalg as LA
 from scipy.misc import factorial
 import random
 
-class Model:
-	"""docstring for Model"""
-	N = 3
-	observations = [(4, 0), (3, 0), (5, 1), (2,0), (9, 2), (11, 1), (8, 1), (20, 2), (17, 2), (18, 3)]
-	# P0 = np.matrix('-.4 .4 0; 0 -.5 .5; 0 0 -.6')
-	P1 = np.eye( N )*.5
-	M = np.matrix('.7 .2 .1 0; .3 .5 .2 0; .1 .2 .1 .6')
-	
-	uniformization_rate = max( [observations[i][0] for i in range(len(observations)) ] )
+class Model():
 
-	# Define a stochastic matrix P0
-	P0 = np.matrix( np.random.rand(N, N) )
-	P0 = P0/P0.sum(axis=1)
+    def __init__(self):
+        """docstring for Model"""
+        self.N = 3
+        self.observations = [(4, 0), (3, 0), (5, 0), (2,0), (9, 2), (11, 2), (8, 2), (20, 3), (17, 3), (18, 3)]
 
-	# Get N elements, each of these less than the respective element on diagonal of P0
-	d = P0.diagonal()
-	d = [random.uniform(0, d[0, i]) for i in range(N)]
+        self.M = np.matrix('.7 .2 .998 0.002; .3 .5 .199 0.001; .1 .2 .1 .6')
 
-	# P1 is exactly a diagonal NxN matrix with d as diagonal
-	P1 = d * np.eye(N)
+        self.uniformization_rate = max( [self.observations[i][0] for i in range(len(self.observations)) ] )
 
-	# P0 + P1 must be stochastic, so:
-	P0 = P0 - P1
+        # self.P0 = np.matrix('.1 .2 .3; 0 .3 .2; 0 0 .6')
+        # self.P1 = np.matrix('.4 0 0; 0 .5 0; 0 0 .4')
+        self.P0, self.P1 = self.generateTransitionProbabilities( self.N )
+
+    def generateTransitionProbabilities(self, N):
+        # Define a stochastic matrix P0
+        P0 = np.matrix(np.random.rand(N, N))
+        P0 = P0 / P0.sum(axis=1)
+
+        # Get N elements, each of these less than the respective element on diagonal of P0
+        d = P0.diagonal()
+        d = [random.uniform(0, d[0, i]) for i in range(N)]
+
+        # P1 is exactly a diagonal NxN matrix with d as diagonal
+        P1 = d * np.eye(N)
+
+        # P0 + P1 must be stochastic, so:
+        P0 = P0 - P1
+
+        return P0, P1
